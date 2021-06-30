c4364a9780a02df033b8760aaf56ee26933f7aed
a.nikitin <a.nikitin@mail.business.ru>
2021-06-30 16:56:45 +0500

    Update AtolClient

--- src/AtolClient.php
+++ src/AtolClient.php
@@ -2,167 +2,139 @@
 
 namespace Atol\Api;
 
+use Predis\Client;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Contracts\HttpClient\ResponseInterface;
-use Symfony\Contracts\HttpClient\ResponseStreamInterface;
 
 /**
- * @method  withOptions(array $options)
+ * Class AtolClient - SDK Atol API
+ * @package Atol\Api
  */
 class AtolClient
 {
 	/**
+	 * Предоставляет гибкие методы для синхронного или асинхронного запроса ресурсов HTTP.
 	 * @var \Symfony\Contracts\HttpClient\HttpClientInterface|null
 	 */
-	protected ?HttpClientInterface $client;
+	private ?HttpClientInterface $client;
 
 	/**
+	 * Url аккаунта Atol API
 	 * @var string
-	 * Url аккаунта
 	 */
-	protected string $account;
+	private string $account;
 
 	/**
-	 * @var string
-	 * Модель аккаунта
-	 */
-	protected string $model;
-
-	/**
-	 * @var false|mixed|null|string
-	 * Токен
-	 */
-	protected $token;
-
-	/**
+	 * Токен аккаунта
 	 * @var string|null
+	 */
+	private ?string $token;
+
+	/**
 	 * Логин пользователя
-	 */
-	protected ?string $userLogin;
-
-	/**
 	 * @var string|null
-	 * Пароль от интеграции
 	 */
-	protected ?string $integrationPassword;
+	private ?string $userLogin;
 
 	/**
-	 * @var object
-	 * Объект для работы с кэшем
+	 * Пароль от интеграции в аккаунте
+	 * @var string|null
 	 */
-	protected object $cache;
+	private ?string $integrationPassword;
 
 	/**
 	 * SymfonyHttpClient constructor.
-	 * @param string $method
-	 * @param string $account
-	 * @param string $model
-	 * @param string|null $login
-	 * @param string|null $password
-	 * @param \Symfony\Contracts\HttpClient\HttpClientInterface|null $decoratedClient
-	 * @throws \Atol\Api\Exception\AtolApiClientException
-	 * @throws \JsonException
+	 * @param string $account - url аккаунта
+	 * @param string|null $login - Логин пользователя
+	 * @param string|null $password -  Пароль интеграции в аккаунте
+	 * @param \Symfony\Contracts\HttpClient\HttpClientInterface|null $client
 	 * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+	 * @throws \JsonException
 	 */
 	public function __construct(
 		string $account,
-		string $model,
 		string $login = null,
 		string $password = null,
 		HttpClientInterface $client = null
 	) {
+		#Получаем логин для токена
+		$this->userLogin = $login;
+		#Получаем пароль для токена
+		$this->integrationPassword = $password;
 		#Подключение к redis
-		$this->cache = RedisAdapter::createConnection('redis://localhost:6379');
+		$cacheRedis = RedisAdapter::createConnection(
+			getenv('REDIS_DSN'),
+			['class' => Client::class, 'timeout' => 3]
+		);
 		#Получаем ссылку от аккаунта
 		$this->account = $account;
-		#Получаем модель
-		$this->model = $model;
-		#Получаем логин пользователя
-		$this->userLogin = $login;
-		#Получаем пароль от интеграции
-		$this->integrationPassword = $password;
-		#Проверяем, есть токен в cache или нет
-		if ($this->cache->exists('tokenCache')) {
-			#Добавляем токен в cache на 24 часа
-			$this->token = $this->cache->setex('tokenCache', 86400, $this->getNewToken());
-		} else {
-			#Получаем текущий токен
-			$this->token = $this->cache->get('tokenCache');
-		}
-		#Подключаем HTTP запросы TODO
+		# HttpClient - выбирает транспорт cURL если расширение PHP cURL включено,
+		# и возвращается к потокам PHP в противном случае
+		# Добавляем в header токен из cache
 		$this->client = $client ?? HttpClient::create(
 				[
 					'http_version' => '2.0',
 					'headers' => [
 						'Content-Type' => 'application/json; charset=utf-8',
-						'Token' => $this->token
+						'Token' => $cacheRedis->get('atolTokenCache')
 					]
 				]
 			);
+		#Проверяем, есть токен в cache или нет
+		if ($cacheRedis->exists('atolTokenCache') === 0) {
+			#Добавляем токен в cache на 10 часов
+			$this->token = $cacheRedis->setex('atolTokenCache', 83000, $this->getNewToken());
+		} else {
+			#Получаем текущий токен
+			$this->token = $cacheRedis->get('atolTokenCache');
+		}
 	}
 
 	/**
+	 * Метод позволяет выполнить запрос к Atol API
 	 * @param string $method - Метод
 	 * @param string $model - Модель
 	 * @param array $params - Параметры
-	 * @return array
-	 * @throws \Atol\Api\Exception\AtolApiClientException
+	 * @return array|string
+	 * @throws \JsonException
 	 * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
-	 * @throws \JsonException
 	 */
-	public function request(string $method, string $model, array $params = []): array
+	public function request(string $method, string $model, array $params = [])
 	{
-		return $this->sendRequest($method, $model, $params);
-	}
-
-	/**
-	 *
-	 * @param string $method
-	 * @param string $model
-	 * @param array $params
-	 * @return array
-	 * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
-	 * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
-	 * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
-	 * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
-	 * @throws \JsonException
-	 */
-	private function sendRequest(string $method, string $model, array $params = []): array
-	{
-		$uri = HttpClient::createForBaseUri($this->account);
-//		var_dump($uri);
-		$request = $uri->request(strtoupper($method), $model, $params);
-		$statusCode = $request->getStatusCode();
+		#Создаем ссылку
+		$url = $this->account . $model;
+		#Отправляем request запрос
+		$response = $this->client->request(
+			strtoupper($method),
+			$url,
+			['json' => $params],
+		);
+		#Получаем статус запроса
+		$statusCode = $response->getStatusCode();
 		if ($statusCode === 200) {
-			$response = $request->getContent();
-			$result = json_decode($response, true, 512, JSON_THROW_ON_ERROR);
-			return $result;
+			return json_decode(
+				$response->getContent(false),
+				true,
+				512,
+				JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT
+			);
 		}
-		if ($statusCode === 401) {
-			return ["Статус запроса" => 401];
-		}
-		return ["status" => "error", "error_code" => "http:" . $statusCode];
-	}
-
-	private function sendTokenRequest(string $method,array $params = []): string
-	{
-		$uri = HttpClient::createForBaseUri("");
+		#false - убрать throw от Symfony.....
+		return $response->toArray(false);
 	}
 
 	/**
 	 * Получаем токен
 	 * @return string
-	 * @throws \Atol\Api\Exception\AtolApiClientException
 	 * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
 	 * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
@@ -170,10 +142,12 @@ class AtolClient
 	 * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
 	 * @throws \JsonException
 	 */
-	protected function getNewToken(): string
+	private function getNewToken(): string
 	{
-		$token = $this->sendTokenRequest(
+		#Получаем новый токен
+		$token = $this->request(
 			"POST",
+			"getToken",
 			[
 				"login" => $this->userLogin,
 				"pass" => $this->integrationPassword
@@ -181,58 +155,5 @@ class AtolClient
 		)["token"];
 		$this->token = $token;
 		return $this->token;
-	}
-
-//	/**
-//	 * Восстановить токен
-//	 * Отправит запрос к API на восстановление токена
-//	 * в случае успеха вернет строку с токеном
-//	 * @return string|array Токен
-//	 * @throws \Atol\Api\Exception\AtolApiClientException
-//	 * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
-//	 * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
-//	 * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
-//	 * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
-//	 * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
-//	 */
-//	protected function getToken(): string
-//	{
-//		$result = $this->sendRequest(
-//			'GET',
-//			$this->account,
-//			'getToken',
-//			[
-//				"login" => $this->userLogin,
-//				"pass" => $this->integrationPassword
-//			]
-//		);
-//		if (isset($result['token']) && is_string($result['token'])) {
-//			$this->token = $result['token'];
-//			var_dump($this->token);
-//			return $this->token;
-//		}
-//
-//		$errorMessage = 'Не удалось получить токен.';
-//
-//		if (is_array($result) && isset($result['error']) && !empty($result['error'])) {
-//			$errorMessage .= ' Код ошибки: ' . $result['error'];
-//		}
-//		throw new AtolApiClientException($errorMessage);
-//	}
-	/**
-	 * Yields responses chunk by chunk as they complete.
-	 *
-	 * @param ResponseInterface|ResponseInterface[]|iterable $responses One or more responses created by the current HTTP client
-	 * @param float|null $timeout The idle timeout before yielding timeout chunks
-	 * @return \Symfony\Contracts\HttpClient\ResponseStreamInterface
-	 */
-	public function stream($responses, float $timeout = null): ResponseStreamInterface
-	{
-		return $this->client->stream($responses, $timeout);
-	}
-
-	public function __call($name, $arguments)
-	{
-		// TODO: Implement @method  withOptions(array $options)
 	}
 }
