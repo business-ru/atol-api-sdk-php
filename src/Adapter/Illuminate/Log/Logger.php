305cb94b487233aac20d97adec52227b93f672d2
a.nikitin <a.nikitin@mail.business.ru>
2023-03-05 13:56:59 +0500

    Изменения под php 8.1 в dev

--- src/Adapter/Illuminate/Log/Logger.php
+++ src/Adapter/Illuminate/Log/Logger.php
@@ -2,11 +2,14 @@
 
 namespace Atol\Api\Adapter\IlluminateAtolApi\Log;
 
-use Monolog\Formatter\LogstashFormatter;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger as MonologLogger;
 use Atol\Api\Adapter\IlluminateAtolApi\Interfaces\LogInterface;
 use Atol\Api\Exception\SimpleLogException;
+use Monolog\DateTimeImmutable;
+use Monolog\Formatter\LogstashFormatter;
+use Monolog\Level;
+use Monolog\Logger as MonologLogger;
+use Monolog\Handler\StreamHandler;
+use Monolog\LogRecord;
 use Psr\Log\InvalidArgumentException;
 
 class Logger implements LogInterface
@@ -28,14 +31,14 @@ class Logger implements LogInterface
      * @var array
      */
     protected array $levels = [
-        'debug' => MonologLogger::DEBUG,
-        'info' => MonologLogger::INFO,
-        'notice' => MonologLogger::NOTICE,
-        'warning' => MonologLogger::WARNING,
-        'error' => MonologLogger::ERROR,
-        'critical' => MonologLogger::CRITICAL,
-        'alert' => MonologLogger::ALERT,
-        'emergency' => MonologLogger::EMERGENCY,
+        'debug' => Level::Debug,
+        'info' => Level::Info,
+        'notice' => Level::Notice,
+        'warning' => Level::Warning,
+        'error' => Level::Error,
+        'critical' => Level::Critical,
+        'alert' => Level::Alert,
+        'emergency' => Level::Emergency,
     ];
 
     /**
@@ -92,13 +95,13 @@ class Logger implements LogInterface
         $logger = $this->logger;
 
         $formatter = new LogstashFormatter('AtolApiSDK');
-        $record = [
-            'level_name' => $level,
-            'level' => $this->level(['level' => $level]),
-            'channel' => 'daily',
-            'message' => $message,
-            'context' => $context
-        ];
+        $record = new LogRecord(
+            datetime: new DateTimeImmutable(true),
+            channel: 'daily',
+            level: MonologLogger::toMonologLevel($level),
+            message: $message,
+            context: $context
+        );
         $formatter->format($record);
         $logFileDaily = $level . '-' . date("Y-m-d");
         $handler = new StreamHandler($this->logPath . "/$logFileDaily.log", $this->level(['level' => $level]));
